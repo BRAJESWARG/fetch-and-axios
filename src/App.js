0c372f004a98950092ef98840adcc5ad7902b01f
Brajeswar Ghosh <brajeswar520@gmail.com>
2022-04-28 17:27:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FetchApiComponent from "./components/fetchApi"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchApiComponent />
+    </div>
+  );
+}
+
+export default App;
